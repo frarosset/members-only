7363342ffca6870a04120ab90441ee8e1c8e5b0f
frarosset <37275581+frarosset@users.noreply.github.com>
2025-11-20 18:44:03 +0100

    feat: implement heartbeat ping-pong to check if ws clients connections are dropped
    Eg, if a client drops its internet connection.

--- websockets/wss.js
+++ websockets/wss.js
@@ -2,6 +2,12 @@ const WebSocket = require("ws");
 const { debouncedWithMaxWaitBroadcast } = require("./broadcast.js");
 const notifier = require("../events/notifier.js");
 
+const wssPingInterval = Number(process.env.WSS_PING_INTERVAL_MIN) * 60000;
+
+function heartbeat(ws) {
+  ws.isAlive = true;
+}
+
 function initWebSocket(server) {
   // This connection is designed to notify connected clients about
   // new messages sent to the server, not to receive messages
@@ -11,6 +17,8 @@ function initWebSocket(server) {
 
   wss.on("connection", (ws) => {
     console.log(`Client connected (tot: ${wss.clients.size})`);
+
+    heartbeat(ws);
 
     // Close client connections if there are too much
     if (wss.clients.size > process.env.MAX_WSS_CLIENTS) {
@@ -25,6 +33,8 @@ function initWebSocket(server) {
     ws.on("error", (error) => {
       console.error("WebSocket error:", error);
     });
+
+    ws.on("pong", () => heartbeat(ws));
   });
 
   // The server emits a simple text to all connected clients only when the database is
@@ -47,6 +57,20 @@ function initWebSocket(server) {
   };
 
   notifier.on("dbUpdated", writeMessage);
+
+  const interval = setInterval(function ping() {
+    console.log(`PING: check ws clients (tot: ${wss.clients.size})`);
+    wss.clients.forEach((ws) => {
+      if (!ws.isAlive) {
+        return ws.terminate();
+      }
+
+      ws.isAlive = false;
+      ws.ping();
+    });
+  }, wssPingInterval);
+
+  wss.on("close", () => clearInterval(interval));
 }
 
 module.exports = initWebSocket;
